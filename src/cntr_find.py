a2768bebbaa1fa005ac320681b3cf0b64c8cec5b
JUHO <ijuho9555@gmail.com>
2025-07-30 11:34:33 +0900

    contour 이용
    contour를 이용해서 각 도형의 외부선을 부각

---
+++ src/cntr_find.py
@@ -0,0 +1,42 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('../img/shapes.png')
+img2 = img.copy()
+
+# 그레이 스케일
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# 스레시홀드로 바이너리 이미지를 만들어서 검은배경에 흰색 전경으로 반전시키기
+ret, imthres = cv2.threshold(imgray, 127, 255, cv2.THRESH_BINARY_INV)
+
+#가장 바깥쪽 컨투어에 대해 모든 좌표 반환
+contour, hierarchy = cv2.findContours(imthres, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+#가장 바깥쪽 컨투어에 대해 꼭지점 좌표만 반환
+contour2, hierarchy = cv2.findContours(imthres, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+# 각각의 컨투의 갯수 출력
+print('도형의 갯수: %d(%d)'% (len(contour), len(contour2)))
+
+# 모든 좌표를 갖는 컨투어 그리기, 초록색 
+cv2.drawContours(img, contour, 1, (0,255,0), 4)
+
+# 꼭지점 좌표만을 갖는 컨투어 그리기, 초록색  
+cv2.drawContours(img2, contour2, 1, (0,255,0), 4)
+
+# 컨투어 모든 좌표를 작은 파랑색 점(원)으로 표시 
+for i in contour:
+    for j in i:
+        cv2.circle(img, tuple(j[0]), 1, (255,0,0), -1) 
+
+# 컨투어 꼭지점 좌표를 작은 파랑색 점(원)으로 표시
+for i in contour2:
+    for j in i:
+        cv2.circle(img2, tuple(j[0]), 1, (255,0,0), -1) 
+
+# 결과 출력 
+cv2.imshow('CHAIN_APPROX_NONE', img)
+cv2.imshow('CHAIN_APPROX_SIMPLE', img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
